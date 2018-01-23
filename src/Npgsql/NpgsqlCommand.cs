627e9e6555bf31ccff49860195b6ea9a80c51cff
Shay Rojansky <roji@roji.org>
2018-01-23 14:38:57 -0800

    Kill all dict lookups

--- src/Npgsql/NpgsqlCommand.cs
+++ src/Npgsql/NpgsqlCommand.cs
@@ -69,7 +69,7 @@ namespace Npgsql
         int? _timeout;
         readonly NpgsqlParameterCollection _parameters;
 
-        readonly List<NpgsqlStatement> _statements;
+        List<NpgsqlStatement> _statements;
 
         /// <summary>
         /// Returns details about each statement that this command has executed.
@@ -573,9 +573,24 @@ GROUP BY pg_proc.proargnames, pg_proc.proargtypes, pg_proc.proallargtypes, pg_pr
                 return Prepare(true);
         }
 
+        [CanBeNull]
+        List<NpgsqlStatement> _crappyCachedStatements;
+
         Task Prepare(bool async)
         {
             var connector = CheckReadyAndGetConnector();
+
+            Log.Debug($"Preparing: {CommandText}", connector.Id);
+
+            // REALLY BAD HACK AHEAD
+            //if (connector.PreparedStatementManager.CommandsBySql.TryGetValue(CommandText, out var statements))
+            if (_crappyCachedStatements != null)
+            {
+                _statements = _crappyCachedStatements;
+                _connectorPreparedOn = connector;
+                return PGUtil.CompletedTask;
+            }
+
             for (var i = 0; i < Parameters.Count; i++)
                 if (!Parameters[i].IsTypeExplicitlySet)
                     throw new InvalidOperationException(
@@ -654,6 +669,11 @@ GROUP BY pg_proc.proargnames, pg_proc.proargtypes, pg_proc.proallargtypes, pg_pr
                 else
                     sendTask.GetAwaiter().GetResult();
             }
+
+            // We've completed preparing. Now cache the entire set of statements for when the same CommandText is used.
+            // TODO: Eject by LRU
+            //connector.PreparedStatementManager.CommandsBySql[CommandText] = _statements;
+            _crappyCachedStatements = _statements;
         }
 
         /// <summary>
