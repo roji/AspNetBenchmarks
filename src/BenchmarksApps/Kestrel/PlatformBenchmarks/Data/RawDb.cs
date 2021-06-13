e0b774c58a0943f948eae6bdd114fbb6a62e01be
Shay Rojansky <roji@roji.org>
2021-06-13 22:25:25 +0200

    Switch to raw SQL for SingleQuery

--- src/BenchmarksApps/Kestrel/PlatformBenchmarks/Data/RawDb.cs
+++ src/BenchmarksApps/Kestrel/PlatformBenchmarks/Data/RawDb.cs
@@ -211,8 +211,8 @@ namespace PlatformBenchmarks
 
         private (NpgsqlCommand readCmd, NpgsqlParameter<int> idParameter) CreateReadCommand(NpgsqlConnection connection)
         {
-            var cmd = new NpgsqlCommand("SELECT id, randomnumber FROM world WHERE id = @Id", connection);
-            var parameter = new NpgsqlParameter<int>(parameterName: "@Id", value: _random.Next(1, 10001));
+            var cmd = new NpgsqlCommand("SELECT id, randomnumber FROM world WHERE id = $1", connection);
+            var parameter = new NpgsqlParameter<int> { TypedValue = _random.Next(1, 10001) };
 
             cmd.Parameters.Add(parameter);
 
